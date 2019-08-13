3fec388055e41949195467b9f6f4303d98b2a082
David Passmore <davidpassmore@gmail.com>
2019-08-12 23:34:34 -0400

    Updates

---
+++ precache-manifest.81ecb748f7312030d86015ed91144c0c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f64deefae3d810184007cceceea686e6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "694877a8088fb9d7f2e6",
+    "url": "/monsters-rolodex/static/css/main.3222bae3.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/monsters-rolodex/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "694877a8088fb9d7f2e6",
+    "url": "/monsters-rolodex/static/js/main.c35a0d1e.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
